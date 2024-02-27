8f8f54e0aaca3e84bc7a6fd395099ffbabc4a150
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-02-27 11:18:58 -0700

    Allow player to choose their role
    Refactor the code to allow player to choose whether to create or guess
    the secret colors. When player is the creator of the secret colors, the
    computer will guess the secret colors with random colors.

--- mastermind.rb
+++ mastermind.rb
@@ -25,42 +25,112 @@ class Color
   end
 end
 
+# Allow player to choose their row
+class Player
+  attr_reader :role
+
+  def initialize
+    @role = choose_role
+  end
+
+  def choose_role
+    role = ''
+    loop do
+      puts 'Which role would you like to play? Enter "1" for Guesser and "2" for Creator'
+      role = gets.chomp.strip
+      break if %w[1 2].include?(role)
+
+      puts 'Invalid input. Please try again.'
+    end
+    role = role == '1' ? 'guesser' : 'creator'
+  end
+end
+
 # Computer will select the secret colors for player to guess it
 class Game
   MAX_TURNS = 12
   def initialize
-    @secret_color = Color.secret_color
+    @player = Player.new
+    @secret_color = []
     @guess = []
   end
 
   def play
-    (1..MAX_TURNS).each do |round|
-      puts '------------------------------------------------------------------------------------'
-      puts "[Round #{round}]"
-
-      enter_guess
-      puts ''
-
-      puts feedback
-      puts ''
-
-      break if correct_guess?
+    if @player.role == 'guesser'
+      play_guesser
+    else
+      play_creator
     end
-    puts result
   end
 
   protected
 
-  def enter_guess
-    puts 'Enter your guess (Leave space between each color, e.g. "Black Yellow Red Blue")'
-    puts "Color options: #{Color.colors}"
+  def play_guesser
+    @secret_color = Color.secret_color
+    (1..MAX_TURNS).each do |round_num|
+      display_layout(round_num, true)
+      break if correct_guess?
+    end
+    puts result(true)
+  end
+
+  def play_creator
+    show_input_remark(false)
+    enter_colors(false)
+    (1..MAX_TURNS).each do |round_num|
+      display_layout(round_num, false)
+      break if correct_guess?
+    end
+    puts result(false)
+  end
+
+  def display_layout(round_num, guess_mode)
+    puts '------------------------------------------------------------------------------------'
+    puts "[Round #{round_num}]"
+
+    if guess_mode
+      show_input_remark(guess_mode)
+      enter_colors(guess_mode)
+    else
+      com_guess
+    end
+    puts ''
+
+    puts feedback
+  end
+
+  def enter_colors(guess_mode)
     loop do
       print '> '
-      @guess = gets.chomp.split.map! { |color| color.strip.downcase }
-      break if @guess.length == 4 && @guess.all? { |color| Color.valid_color?(color) }
-
-      puts 'Invalid input. Please try again.'
+      if guess_mode
+        @guess = gets.chomp.split.map! { |color| color.strip.downcase }
+      else
+        @secret_color = gets.chomp.split.map! { |color| color.strip.downcase }
+      end
+      break if valid_input?(guess_mode)
     end
+  end
+
+  def com_guess
+    @guess = Color.secret_color
+    puts "Computer guess: #{@guess.join(' ')}"
+  end
+
+  def show_input_remark(guess_mode)
+    print "Enter #{guess_mode ? 'your guess' : 'the secret colors'} "
+    puts '(Leave space between each color, e.g. "Black Yellow Red Blue")'
+    puts "Color options: #{Color.colors}"
+  end
+
+  def valid_input?(guess_mode)
+    check_var = guess_mode ? @guess : @secret_color
+    validity = check_var.length == 4 && check_var.all? { |color| Color.valid_color?(color) }
+    puts 'Invalid input. Please try again' unless validity
+    validity
+  end
+
+  def feedback
+    "Correct Position: #{correct_position} | Correct Color: #{correct_color}"
   end
 
   def correct_position
@@ -81,15 +151,15 @@ class Game
     same_color - correct_position
   end
 
-  def feedback
-    "Correct Position: #{correct_position} | Correct Color: #{correct_color}"
-  end
-
   def correct_guess?
     correct_position == 4
   end
 
-  def result
-    correct_guess? ? 'Congrat! You got the correct answer!' : "Oops! You can't get the secret color. Let's play again!"
+  def result(guess_mode)
+    if guess_mode
+      correct_guess? ? 'Congrat! You got the correct answer!' : "Oops! You can't get the secret color."
+    else
+      correct_guess? ? 'Computer got the correct answer!' : "Oops! Computer can't get the secret color."
+    end
   end
 end
